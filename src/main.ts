36855195adaafa938771d57ad14a0a194538d8fb
Andrew Smirnov <andrei.smirnov@crplab.ru>
2021-10-26 13:09:33 +0300

    init

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
+// @ts-nocheck
 import { createApp } from 'vue';
 import App from './App.vue';
 import { router } from './router';
 import { store } from './store';
+import PrimeVue from 'primevue/config';
+import Tooltip from 'primevue/tooltip';
+
+import { LOCALE_RU } from '@/dictionaries/locale/locale.ru';
+
+import('primevue/resources/themes/saga-blue/theme.css');
+import('primevue/resources/primevue.min.css');
+import('primeicons/primeicons.css');
+import('primeflex/primeflex.scss');
 
 createApp(App)
+  .use(PrimeVue, {
+    ripple: true,
+    locale: LOCALE_RU
+  })
   .use(store)
   .use(router)
+  .directive('tooltip', Tooltip)
   .mount('#app');
